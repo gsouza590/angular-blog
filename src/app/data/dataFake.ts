03eb836ab9c46d92e6558a8d0993d93f4752988a
gsouza590 <gsouza590@gmail.com>
2023-01-24 15:08:19 -0300

    Adicionando conteudo dinamico

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,25 @@
+export const dataFake =[
+ {"id":"1",
+"photo":"https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg",
+"title": "Saiu nova noticia",    
+"description": "Descricao da noticia fake"
+},
+
+{"id":"2",
+"photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeNu6LNlQdPm7r0k6mc68quF7h4hFC7ta0Pw&usqp=CAU",
+"title": "Saiu nova noticia",    
+"description": "Descricao da noticia fake"
+},
+
+{"id":"3",
+"photo":"https://upload.wikimedia.org/wikipedia/pt/thumb/0/06/TW3_Wild_Hunt.png/270px-TW3_Wild_Hunt.png",
+"title": "Saiu nova noticia",    
+"description": "Descricao da noticia fake"
+},
+
+{"id":"4",
+"photo":"https://store-images.s-microsoft.com/image/apps.59172.66474211586744867.e24f67e1-7195-4bf7-9758-84e7723c0673.f1ac78b0-3728-4d7f-8ccd-11a5398dbdfa",
+"title": "Saiu nova noticia",    
+"description": "Descricao da noticia fake"
+}
+]
